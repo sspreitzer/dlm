81be66a3df6a6bebc7b1ad9a758778164546b4be
Sascha Spreitzer <sascha@redhat.com>
2018-05-14 09:07:46 +0200

    Add initial code

---
+++ dlm.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'dlm'
+  s.version     = '0.1.0'
+  s.date        = '2018-05-14'
+  s.summary     = "Distributed Lock Manager"
+  s.description = "Conservative redis based ruby distributed lock manager"
+  s.authors     = ["Sascha Spreitzer"]
+  s.email       = 'sascha@redhat.com'
+  s.files       = ["dlm.rb"]
+  s.homepage    =
+    'http://github.com/sspreitzer/dlm'
+  s.license       = 'MIT'
+end
---
+++ dlm.rb
@@ -0,0 +1,38 @@
+require 'redis'
+
+class DLMLockTaken < StandardError
+end
+
+class DLM
+  def initialize(redis=Redis.new)
+    @redis = redis
+  end
+
+  def lock(what, block = false, lockinfo = 1)
+    if block
+      self.wait(what)
+    end
+    ret = @redis.set("dlm_#{what}", lockinfo, :nx => true)
+    raise DLMLockTaken, "Lock is already taken for #{what}: dlm_#{what}" unless ret
+  end
+
+  def wait(what)
+    while ! @redis.get("dlm_#{what}").nil?
+      sleep(0.1)
+    end
+  end
+
+  def lockinfo(what)
+    @redis.get("dlm_#{what}")
+  end
+
+  def unlock(what)
+    @redis.del("dlm_#{what}")
+  end
+
+  def unlockall
+    keys = @redis.keys("dlm_*")
+    @redis.del(keys) unless keys.empty?
+  end
+
+end
